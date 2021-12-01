872421d0fe75e8e6faba614c48867f9241994d1e
maanoel <manoelvitorbrito@gmail.com>
2021-11-30 22:28:44 -0300

    finalizado

--- CasaDoCodigoDois/Startup.cs
+++ CasaDoCodigoDois/Startup.cs
@@ -12,7 +12,15 @@ namespace CasaDoCodigoDois
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddTransient<ICatalogo, Catalogo>();
+      //services.AddTransient<ICatalogo, Catalogo>();
+      //services.AddTransient<IRelatorio, Relatorio>();
+
+      //services.AddScoped<ICatalogo, Catalogo>();
+      //services.AddScoped<IRelatorio, Relatorio>();
+
+      var catalogo = new Catalogo();
+      services.AddSingleton<ICatalogo>(catalogo);
+      services.AddSingleton<IRelatorio>(new Relatorio(catalogo));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +34,7 @@ namespace CasaDoCodigoDois
       app.UseRouting();
 
       ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();
-      IRelatorio relatorio = new Relatorio(catalogo);
+      IRelatorio relatorio = serviceProvider.GetService<IRelatorio>();
 
       app.UseEndpoints(endpoints =>
       {
